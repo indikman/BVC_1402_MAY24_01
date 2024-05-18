e31154ceac8dc1dd3b4e649b147b3bce7c655d0a
Indika Wijesooriya <indikman@gmail.com>
2024-05-17 19:22:22 -0600

    Class 1

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransform;
+    
+    [Header("Camera Speeds")]
+    [SerializeField] private float cameraFollowSpeed = 0.2f;
+    [SerializeField] private float cameraLookSpeed = 2f;
+    [SerializeField] private float cameraPivotSpeed = 2f;
+
+    [Header("Camera Pivot")]
+    [SerializeField] private float minPivotAngle = -35f;
+    [SerializeField] private float maxPivotAngle = 35f;
+    [SerializeField] private Transform cameraPivot;
+    
+    private Vector3 _cameraFollowVelocity = Vector3.zero;
+
+    private float _lookAngle = 0f;
+    private float _pivotAngle = 0f;
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
+    void Update()
+    {
+        FollowTarget();
+    }
+
+    private void FollowTarget()
+    {
+        Vector3 targetPosition = Vector3.SmoothDamp(transform.position, targetTransform.position,
+            ref _cameraFollowVelocity, cameraFollowSpeed);
+        transform.position = targetPosition;
+    }
+
+    public void RotateCamera(Vector2 mouseInput)
+    {
+        Vector3 lookRotation = Vector3.zero;
+        Vector3 pivotRotation = Vector3.zero;
+        
+        //LOOKING
+        _lookAngle = _lookAngle + mouseInput.x * cameraLookSpeed;
+        lookRotation.y = _lookAngle;
+
+        Quaternion targetLookRotation = Quaternion.Euler(lookRotation);
+        transform.rotation = targetLookRotation;
+        
+        //PIVOT
+        _pivotAngle = _pivotAngle - mouseInput.y * cameraPivotSpeed;
+        _pivotAngle = Mathf.Clamp(_pivotAngle, minPivotAngle, maxPivotAngle);
+        pivotRotation.x = _pivotAngle;
+
+        Quaternion targetPivotRotation = Quaternion.Euler(pivotRotation);
+        cameraPivot.localRotation = targetPivotRotation;
+
+    }
+    
+}
